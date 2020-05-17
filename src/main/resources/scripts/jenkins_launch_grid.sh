55ce2bc63ae43b0ce12af40ffef92f7e60607a96
sergii <sergiistepanov85@gmail.com>
2020-05-17 06:49:59 +0200

    start selenium-server-standalone-3.141.59.jar on Jenkins in background

--- src/main/resources/scripts/jenkins_launch_grid.sh
+++ src/main/resources/scripts/jenkins_launch_grid.sh
@@ -10,11 +10,16 @@ echo "Starting ${COMMAND_NAME} .........."
 
 #java -jar ./gridConfig/selenium-server-standalone-3.141.59.jar -role hub -hubConfig ./gridConfig/hubConfig.json > /dev/null 2>&1 &
 
-
 #  > /dev/null 2>&1 &  - execute in background mode
 echo "Executing comand: $COMMAND"
-#$COMMAND > /dev/null 2>&1 &
-ls
+$COMMAND > /dev/null 2>&1 &
+sleep 3
+
+CURL='/usr/bin/curl'
+RVMHTTP="http://localhost:4444/wd/hub/status"
+CURLARGS="-f -s -S -k"
+raw="$($CURL $RVMHTTP)"
+echo $raw
 }
 
 SELENIUM_SERVER_FILE="selenium-server-standalone-3.141.59.jar"

9e41c5d272ae03884354ab9dc01a02a475b1f523
sergii <sergiistepanov85@gmail.com>
2020-05-27 06:31:58 +0200

    start selenium grid configuration on Jenkins

--- src/main/resources/scripts/jenkins_launch_grid.sh
+++ src/main/resources/scripts/jenkins_launch_grid.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
+java -jar ./gridConfig/selenium-server-standalone-3.141.59.jar -role hub -hubConfig ./gridConfig/hubConfig.json > /dev/null 2>&1 &
 
 function new_tab() {
   COMMAND_NAME=$1
@@ -13,11 +14,11 @@ echo "Starting ${COMMAND_NAME} .........."
 
 #  > /dev/null 2>&1 &  - execute in background mode
 echo "Executing comand: $COMMAND"
-$COMMAND > /dev/null 2>&1 &
+#$COMMAND > /dev/null 2>&1 &
 sleep 3
 
 CURL='/usr/bin/curl'
-RVMHTTP="http://localhost:4444/wd/hub/status"
+RVMHTTP="http://127.0.0.1:4723/wd/hub/status"
 CURLARGS="-f -s -S -k"
 raw="$($CURL $RVMHTTP)"
 echo $raw
@@ -27,5 +28,5 @@ SELENIUM_SERVER_FILE="selenium-server-standalone-3.141.59.jar"
 CONFIG_PATH="./gridConfig"
 
 new_tab "Selenium Grid" "java -jar $CONFIG_PATH/$SELENIUM_SERVER_FILE -role hub -hubConfig $CONFIG_PATH/hubConfig.json"
-#new_tab "Node1-iPhone8" "appium --port 4723 --nodeconfig $CONFIG_PATH/nodeConfigiPhone8.json --session-override"
-#new_tab "Node2-GalaxyS10" "appium --port 4724 --nodeconfig $CONFIG_PATH/nodeConfigGalaxyS10.json --session-override"
+new_tab "Node1-iPhone8" "appium --port 4723 --nodeconfig $CONFIG_PATH/nodeConfigiPhone8.json --session-override"
+new_tab "Node2-GalaxyS10" "appium --port 4724 --nodeconfig $CONFIG_PATH/nodeConfigGalaxyS10.json --session-override"
